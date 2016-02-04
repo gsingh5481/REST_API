9068790e81ff282c0346b7fe42aaa36d89ec6906
gsingh5481 <studentsabi@gmail.com>
2016-02-03 22:47:31 -0500

    wip

--- model/userModel.js
+++ model/userModel.js
@@ -12,4 +12,18 @@ mongoose.model('users',userSchema);
     mongoose.model('uses',function(err,users){
     res.send(users);
   })
+
+
+
+
+
+  //to read file and laod it
+
+  var fs = require('fs');
+
+  fs.readdirSync(__dirname + '/models').forEach(function(filename){
+  if (~filename.indexOf('.js'))  //dont have to do this, but to check this if
+                                // the file is js, because this will load all files;
+    require(__dirname + 'models' + filename);
+});
 */
